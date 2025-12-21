a913e2cc252e97fdf9cb8c2a207e7b4057bb1d1c
karthick-krt <karthicksreeni.krt@gmail.com>
2025-12-21 21:05:07 +0530

    React small integration

--- application/src/main/java/com/syslogmanager/application/controller/ApplicationController.java
+++ application/src/main/java/com/syslogmanager/application/controller/ApplicationController.java
@@ -1,13 +1,17 @@
-package com.syslogmanager.application;
+package com.syslogmanager.application.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ApplicationController {
     
     @GetMapping("/home")
-    public String getHome() {
+    public String home() {
         return "Syslog Manager Application is running.";
     }
     
